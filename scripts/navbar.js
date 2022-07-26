18195a6dc45991788be1aa48a211eee45bf33693
mule <belmeetmule@gmail.com>
2022-07-27 02:33:19 +0300

    fix linter errors

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,26 +1,26 @@
-const menu = document.querySelector(".menu")
-const menuItems = document.querySelectorAll(".menuItem")
-const closeIcon = document.querySelector(".closeIcon")
-const menuIcon = document.querySelector(".menuIcon");
-const navHeader = document.querySelector("header")
+const menu = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menuItem');
+const closeIcon = document.querySelector('.closeIcon');
+const menuIcon = document.querySelector('.menuIcon');
+const navHeader = document.querySelector('header');
 
 function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-    navHeader.style.position="fixed"
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+    navHeader.style.position = 'fixed';
   } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-    navHeader.style.position="relative"
+    menu.classList.add('showMenu');
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
+    navHeader.style.position = 'relative';
   }
 }
-  closeIcon.addEventListener("click",toggleMenu);
-  menuIcon.addEventListener("click",toggleMenu);
+closeIcon.addEventListener('click', toggleMenu);
+menuIcon.addEventListener('click', toggleMenu);
 
-  menuItems.forEach(function(menuItem){
-    menuItem.addEventListener("click", toggleMenu);
-  });
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener('click', toggleMenu);
+});
 //
