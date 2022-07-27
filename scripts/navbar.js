2577ec14018700d2cae70564aa810a3f38e542bc
Abrar052 <bc160400820@vu.edu.pk>
2022-07-27 19:50:42 +0500

    Remove repeated code

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,5 +1,4 @@
 const menu = document.querySelector('.menu');
-const menuItems = document.querySelectorAll('.menuItem');
 const closeIcon = document.querySelector('.closeIcon');
 const menuIcon = document.querySelector('.menuIcon');
 const navHeader = document.querySelector('header');
