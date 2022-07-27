ea039168c41d0c1d877e57206137f71d372ca00e
Abrar052 <bc160400820@vu.edu.pk>
2022-07-28 01:48:48 +0500

    Add Project details into project array

---
+++ scripts/pop-up.js
@@ -0,0 +1,70 @@
+const projects = [
+    {
+      id: 'project-1',
+      name: 'Tonic',
+      featured_image: 'images/Portfolio-1.png',
+      sub: [
+        'CANOPY',
+        '',
+        'Back End Dev',
+        '',
+        '2015',
+      ],
+      langs: [
+        'html',
+        'css',
+        'javascript',
+      ],
+      live_link: '',
+      source_link: '',
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    },
+    {
+        id: 'project-3',
+        name: 'Facebook 360',
+        featured_image: 'images/Portfolio-3.png',
+        sub: [
+          'CANOPY',
+          '',
+          'Back End Dev',
+          '',
+          '2015',
+        ],
+        langs: [
+          'html',
+          'css',
+          'javascript',
+        ],
+        live_link: '',
+        source_link: '',
+        description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    },
+    {
+       id: 'project-4',
+       name: 'Uber Navigation',
+       featured_image: 'images/Portfolio-4.png',
+       sub: [
+         'CANOPY',
+         '',
+         'Back End Dev',
+         '',
+         '2015',
+       ],
+          
+       langs: [
+         'html',
+         'css',
+         'javascript',
+       ],
+          
+       live_link: '',
+       source_link: '',
+       description:
+       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+     },
+            ];
+          
+        
+
