0f6b6c7ca7bda475dc3408f8d5100ed43dff84fe
mule <belmeetmule@gmail.com>
2022-07-29 01:53:50 +0300

    add script for the form validation

--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -1,3 +1,15 @@
 const form = document.forms[0];
 const email = document.getElementById('email');
 const error = document.getElementById('error');
+const message = document.querySelector('.error-message');
+console.log(message)
+form.addEventListener('submit', (event) => {
+  const messageWhileError = [];
+  if (email.value.match(/[A-Z]/)) {
+    messageWhileError.push('Email should be written in lowercase letters.');
+    error.innerHTML = messageWhileError.join(', ');
+    message.style.display="block";
+    event.preventDefault();
+    event.stopPropagation();
+  }
+});
