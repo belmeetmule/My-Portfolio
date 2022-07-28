6137aee066f8e7959932a915bf40b415d3d83b5d
Abrar052 <bc160400820@vu.edu.pk>
2022-07-28 22:46:17 +0500

    Add function to collect data from form

--- scripts/local-store.js
+++ scripts/local-store.js
@@ -1,2 +1,10 @@
 const contactForm = document.forms[0];
 const formValues = {};
+
+function collectFormData() {
+    formValues.name = contactForm.name.value;
+    formValues.email = contactForm.email.value;
+    formValues.message = contactForm.message.value;
+    window.localStorage.setItem('show data', JSON.stringify(formValues));
+  }
+  
